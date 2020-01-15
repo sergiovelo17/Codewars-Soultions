11f4ade2d09216a6400d541d55c16a35ea724fee
sergiovelo17 <sergiovelo17@gmail.com>
2020-01-15 18:11:40 -0500

    added codewars

--- solutions.js
+++ solutions.js
@@ -70,6 +70,15 @@ function permuteAPalindrome (input) {
     return this.spells.length;
     }
     }
+    //get middle character
+    function getMiddle(s)
+    {
+      if(s.length %2 == 0){
+        return "" + s.charAt(s.length/2-1) + s.charAt((s.length/2));
+      }else{
+       return "" + s.charAt(Math.floor(s.length/2));
+    }
+}
 
     //get character count
     function letterFrequency(text){
