efd5a13cd6d8d4ebdf29acf28d79d4f166290160
sergiovelo17 <sergiovelo17@gmail.com>
2020-02-10 18:55:01 -0500

    find the mine code wars challenge added

--- solutions.js
+++ solutions.js
@@ -442,4 +442,13 @@ const makeChange = (amount) => {
       }
         return primes;
     }
-    
+    //find the mine given a 2d map
+    function mineLocation(field){
+      for(let i = 0; i < field.length; i++){
+        for(let j = 0; j < field[0].length; j++){
+          if(field[i][j] == 1){
+            return [i,j]
+          }
+        }
+      }
+    }
