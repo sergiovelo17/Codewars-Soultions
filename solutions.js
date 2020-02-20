5f35c869d6d265e1c5896f6129348f34de92d2b7
sergiovelo17 <sergiovelo17@gmail.com>
2020-02-20 16:46:28 -0500

    work from 2/20

--- solutions.js
+++ solutions.js
@@ -589,3 +589,45 @@ function narcissistic(value) {
   }
   return total == value ? true : false;
 }
+//see if patient fears a number given day of weak and their condition
+var AmIAfraid = function(day, num){
+  switch(day){
+  case "Monday":
+  if(num == 12){
+  return true;
+  }
+  break;
+  case "Tuesday":
+  if(num > 95){
+  return true;
+  }
+  break;
+  case "Wednesday":
+  if(num == 34){
+  return true;
+  }
+  break;
+  case "Thursday":
+  if(num == 0){
+  return true;
+  }
+  break;
+  case "Friday":
+  if(num % 2 == 0){
+  return true;
+  }
+  break;
+  case "Saturday":
+  if(num == 56){
+  return true;
+  }
+  break;
+  case "Sunday":
+  if(num == 666 || num == -666){
+  return true;
+  }
+  break;
+  }
+  return false;
+}
+
